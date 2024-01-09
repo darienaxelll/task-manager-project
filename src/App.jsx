a4283b6a5ec39e52a496d88caa6eb15919d6e1c4
Darien Salazar <darien.axell.salazar@gmail.com>
2024-01-08 20:11:35 -0600

    Seperated projectForm from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import logo from "./assets/no-projects.png";
+import ProjectForm from "./components/ProjectForm";
 
 function App() {
   const [newProject, setNewProject] = useState(false);
+
+  let people = [];
+
+
 
   const handleNewProject = () => {
     setNewProject((prevState) => !prevState);
@@ -12,6 +17,7 @@ function App() {
     e.preventDefault();
     setNewProject(false);
   };
+
 
   const noProject = (
     <div className="mt-24 text-center w-2/3">
@@ -32,55 +38,6 @@ function App() {
     </div>
   );
 
-  const projectForm = (
-    <div className="mt-24  w-2/3">
-      <form>
-        <menu className="flex items-center justify-end gap-4 my-4">
-          <button
-            className="text-stone-800 hover:text-stone-950"
-            onClick={handleCancel}
-          >
-            Cancel
-          </button>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
-            Save
-          </button>
-        </menu>
-
-        <label
-          htmlFor="title"
-          className="text-sm font-bold uppercase text-stone-500"
-        >
-          Title
-        </label>
-        <input
-          id="title"
-          className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
-        />
-        <label
-          htmlFor="description"
-          className="text-sm font-bold uppercase text-stone-500"
-        >
-          Description
-        </label>
-        <input
-          id="description"
-          className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
-        />
-        <label
-          htmlFor="date"
-          className="text-sm font-bold uppercase text-stone-500"
-        >
-          Due Date
-        </label>
-        <input
-          id="date"
-          type="date"
-          className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
-        />
-      </form>
-    </div>
-  );
 
   return (
     <main className="h-screen my-8 flex gap-8">
@@ -97,7 +54,7 @@ function App() {
       </aside>
 
       {!newProject && noProject}
-      {newProject && projectForm}
+      {newProject && <ProjectForm onCancel={handleCancel}/>}
     </main>
   );
 }
--- src/components/ProjectForm.jsx
+++ src/components/ProjectForm.jsx
@@ -1,42 +1,53 @@
-const ProjectForm = () => {
+const ProjectForm = (props) => {
+
   return (
-    <form>
-      <button className="text-stone-800 hover:text-stone-950">Cancel</button>
-      <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
-        Save
-      </button>
-      <label
-        htmlFor="title"
-        className="text-sm font-bold uppercase text-stone-500"
-      >
-        <p className="flex flex-col gap-1 my-4">TITLE</p>
-      </label>
-      <input
-        id="title"
-        className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
-      />
-      <label
-        htmlFor="description"
-        className="text-sm font-bold uppercase text-stone-500"
-      >
-        <p className="flex flex-col gap-1 my-4">DESCRIPTION</p>
-      </label>
-      <input
-        id="description"
-        className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
-      />
-      <label
-        htmlFor="date"
-        className="text-sm font-bold uppercase text-stone-500"
-      >
-        <p className="flex flex-col gap-1 my-4">DUE DATE</p>
-      </label>
-      <input
-        id="date"
-        type="date"
-        className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
-      />
-    </form>
+    <div className="mt-24  w-2/3">
+      <form>
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <button
+            className="text-stone-800 hover:text-stone-950"
+            onClick={props.onCancel}
+          >
+            Cancel
+          </button>
+          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+            Save
+          </button>
+        </menu>
+
+        <label
+          htmlFor="title"
+          className="text-sm font-bold uppercase text-stone-500"
+        >
+          Title
+        </label>
+        <input
+          id="title"
+          className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+        />
+        <label
+          htmlFor="description"
+          className="text-sm font-bold uppercase text-stone-500"
+        >
+          Description
+        </label>
+        <input
+          id="description"
+          className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+        />
+        <label
+          htmlFor="date"
+          className="text-sm font-bold uppercase text-stone-500"
+        >
+          Due Date
+        </label>
+        <input
+          id="date"
+          type="date"
+          className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+        />
+      </form>
+    </div>
   );
 };
 
